c00416b1f9ca16996ec3e03f30e1d3526925be4c
SharifH <sharifhadidi@gmail.com>
2014-06-09 11:07:26 -0700

    got it up and running

--- js/main.js
+++ js/main.js
@@ -59,3 +59,24 @@
 
 })();
 
+$(function(){
+var timer = setInterval(function(){
+$("#count_num").html(function(i,html){
+
+if(parseInt(html)>0)
+   {
+   return parseInt(html)-1;
+   }
+   else
+   {
+   clearTimeout(timer);
+       return "KO!!";
+   }
+ });
+
+},1000);
+
+
+
+});
+
